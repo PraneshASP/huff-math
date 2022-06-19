a23fb2ac4e582cd59e2361ea0f80f9f8c5461c72
PraneshASP <praneshas2000@gmail.com>
2022-06-19 15:40:23 +0530

    ♻️ Add 'abs()' method

--- test/Math.t.js
+++ test/Math.t.js
@@ -1,6 +1,9 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
+const OVERLOW =
+  "115792089237316195423570985008687907853269984665640564039457584007913129639933";
+
 describe("Math", function () {
   beforeEach(async function () {
     const Math = await ethers.getContractFactory("Math");
@@ -29,5 +32,14 @@ describe("Math", function () {
     let expected = num1 > num2 ? OVERLOW : num2 - num1;
     await math.subNumbers(num1, num2);
     expect(await math.callStatic.subNumbers(num1, num2)).to.be.equal(expected);
+  });
+
+  it("Performs abs", async function () {
+    let num1 = 1990,
+      num2 = 188888;
+    await math.abs(num1, num2);
+    expect(await math.callStatic.abs(num1, num2)).to.be.equal(
+      Math.abs(num1 - num2)
+    );
   });
 });
