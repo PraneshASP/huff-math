527ec3879207692182ac4c04b8d16ad08169a8e4
PraneshASP <praneshas2000@gmail.com>
2022-06-21 21:56:00 +0530

    ♻️ Add Fuzz tests

--- test/Math.t.sol
+++ test/Math.t.sol
@@ -1,6 +1,54 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract MathTest {
-    function setUp() public {}
+import "forge-std/Test.sol";
+
+/// @notice Interface for the Math contract
+interface IMath {
+    function addNumbers(uint256, uint256) external view returns (uint256);
+
+    function subNumbers(uint256, uint256) external view returns (uint256);
+
+    function multiplyNumbers(uint256, uint256) external view returns (uint256);
+
+    function divideNumbers(uint256, uint256) external view returns (uint256);
+
+    function abs(uint256, uint256) external view returns (uint256);
+}
+
+contract MathTest is Test {
+    IMath public math;
+
+    uint256 public constant MAX = type(uint256).max;
+
+    function setUp() public {
+        // Hardcoding the deployed address as there are some issues with --ffi and huffc
+        math = IMath(0x10ec4df6A9C43b393fcfc39D639C09EBCa0a9b5d);
+    }
+
+    function testAdd(uint256 a, uint256 b) public {
+        // overflow not handled in the contract
+        vm.assume(a < MAX / 2 && b < MAX / 2);
+        require(math.addNumbers(a, b) == a + b);
+    }
+
+    function testSub(uint256 a, uint256 b) public {
+        vm.assume(b > a);
+        require(math.subNumbers(a, b) == b - a);
+    }
+
+    function testMul(uint256 a, uint256 b) public {
+        vm.assume((b > 0 && a < MAX / b) || (a > 0 && b < MAX / a));
+        require(math.multiplyNumbers(a, b) == a * b);
+    }
+
+    function testDiv(uint256 a, uint256 b) public {
+        vm.assume(a > 0);
+        require(math.divideNumbers(a, b) == b / a);
+    }
+
+    function testAbs(uint256 a, uint256 b) public view {
+        uint256 _result = a > b ? a - b : b - a;
+        require(math.abs(a, b) == _result);
+    }
 }
