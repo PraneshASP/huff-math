c145d4d0031f9b5562cf2d02b3b7aba36ef494ee
PraneshASP <praneshas2000@gmail.com>
2022-08-30 22:51:57 +0530

    ♻ Refactor 'wadToRay()' & 'rayToWad()' wrappers and tests

--- test/WadRayMath.t.sol
+++ test/WadRayMath.t.sol
@@ -68,35 +68,35 @@ contract WadRayMathTest is Test {
         assertEq(sut.rayMul(0, 0), 0);
     }
 
-    // function testRayMul() public {
-    //     assertEq(sut.rayMul(2.5e27, 0.5e27), 1.25e27);
-    //     assertEq(sut.rayMul(3e27, 1e27), 3e27);
-    //     assertEq(sut.rayMul(369, 271), 0);
-    // }
+    function testRayMul() public {
+        assertEq(sut.rayMul(2.5e27, 0.5e27), 1.25e27);
+        assertEq(sut.rayMul(3e27, 1e27), 3e27);
+        assertEq(sut.rayMul(369, 271), 0);
+    }
 
-    // function testDivRayDown() public {
-    //     assertEq(sut.rayDiv(1.25e27, 0.5e27), 2.5e27);
-    //     assertEq(sut.rayDiv(3e27, 1e27), 3e27);
-    //     assertEq(sut.rayDiv(2, 100000000000000e27), 0);
-    // }
+    function testDivRayDown() public {
+        assertEq(sut.rayDiv(1.25e27, 0.5e27), 2.5e27);
+        assertEq(sut.rayDiv(3e27, 1e27), 3e27);
+        assertEq(sut.rayDiv(2, 100000000000000e27), 0);
+    }
 
-    // function testDivRayDownEdgeCases() public {
-    //     assertEq(sut.rayDiv(0, 1e27), 0);
-    // }
+    function testDivRayDownEdgeCases() public {
+        assertEq(sut.rayDiv(0, 1e27), 0);
+    }
 
-    // function testFailDivRayDownZeroDenominator() public {
-    //     sut.rayDiv(1e27, 0);
-    // }
+    function testFailDivRayDownZeroDenominator() public {
+        sut.rayDiv(1e27, 0);
+    }
 
-    // function testRayToWad() public {
-    //     assertEq(sut.rayToWad(0), 0);
-    //     assertEq(sut.rayToWad(1e27), 1e18);
-    //     assertEq(sut.rayToWad(50000e27), 50000e18);
-    // }
+    function testRayToWad() public {
+        assertEq(sut.rayToWad(0), 0);
+        assertEq(sut.rayToWad(1e27), 1e18);
+        assertEq(sut.rayToWad(50000e27), 50000e18);
+    }
 
-    // function testWadToRay() public {
-    //     assertEq(sut.wadToRay(0), 0);
-    //     assertEq(sut.wadToRay(1e18), 1e27);
-    //     assertEq(sut.wadToRay(50000e18), 50000e27);
-    // }
+    function testWadToRay() public {
+        assertEq(sut.wadToRay(0), 0);
+        assertEq(sut.wadToRay(1e18), 1e27);
+        assertEq(sut.wadToRay(50000e18), 50000e27);
+    }
 }
