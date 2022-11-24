a1b8cde7edd7c64b5845e84f9aaa2c91182eb7a1
PraneshASP <praneshas2000@gmail.com>
2022-11-24 17:10:25 +0800

    🚚  move IMath interface

---
+++ src/interfaces/IMath.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+/// @notice Interface for the Math contract
+interface IMath {
+    function addNumbers(uint256, uint256) external view returns (uint256);
+
+    function subNumbers(uint256, uint256) external view returns (uint256);
+
+    function multiplyNumbers(uint256, uint256) external view returns (uint256);
+
+    function divideNumbers(uint256, uint256) external view returns (uint256);
+
+    function abs(uint256, uint256) external view returns (uint256);
+}
